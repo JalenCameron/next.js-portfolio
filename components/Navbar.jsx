e16ed31b8b0c986fcf4a436af9bb295da69fafa9
JalenCameron <109976308+JalenCameron@users.noreply.github.com>
2022-10-17 00:02:24 -0700

    slight styling for dropdown menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -27,10 +27,10 @@ const Navbar = () => {
                 </label>
             </div>
 
-            <ul className={`menu md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-[#f1f3f4] md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 opacity-100' : 'top-[490px]'} md:opacity-100`}>
+            <ul className={`menu md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-[#f1f3f4] md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-12 transition-all duration-500 ease-in ${open ? 'top-20 opacity-100' : 'top-[490px]'} md:opacity-100`}>
                 {
                     Links.map((link) => (
-                        <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
+                        <li key={link.name} className="md:ml-8 text-3xl md:my-0 my-7">
                             <Link href={link.link}>
                                 <a alt={`${link.name} Page`} className="hover:text-[#7f96bb] duration-300">{link.name}</a>
                             </Link>
