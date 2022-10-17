64f00f3c58da18e9db39b929d0dff174ef392012
JalenCameron <109976308+JalenCameron@users.noreply.github.com>
2022-10-16 23:44:25 -0700

    Added some global styling to create a more dynamic Navigation Menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { CgClose, CgMenuRightAlt  } from 'react-icons/cg';
 import { useState } from 'react';
 
 const Navbar = () => {
@@ -11,21 +10,24 @@ const Navbar = () => {
         {name:"Works", link:"/"},
         {name:"Contact", link:"/"},
     ]
-
+    // Creating an Open state in order to have my Navigation menu dynamically appear
     let [open, setOpen] = useState(false);
 
   return (
     <div className='shadow-md w-full fixed top-0 left-0'>
-        <div className='md:flex items-center justify-between bg-[#f1f3f4] py-4 md:px-10 px-7'>
+        <nav className='header md:flex items-center justify-between bg-[#f1f3f4] py-4 md:px-10 px-7'>
             <div className='cursor-pointer'>
-                <Image src="/../public/assets/logo-black.png" alt="The letter J being used as an Icon" width="60" height="50" />
+                <Image src="/assets/logo-black.png" alt="The letter J being used as an Icon" width="60" height="50" />
             </div>
 
-            <div onClick={() => setOpen(!open)} className='text-4xl absolute right-8 top-6 cursor-pointer md:hidden'>
-                {open ? <CgClose /> : <CgMenuRightAlt />}
+            <div className='text-4xl absolute right-4 top-4 cursor-pointer md:hidden'>
+                <input className="dropdown-button" type="checkbox" id="dropdown-button" onClick={() => setOpen(!open)} />
+                <label className="menu-icon" htmlFor="dropdown-button">
+                    <span className="hamburger"></span>
+                </label>
             </div>
 
-            <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-[#f1f3f4] md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 opacity-100' : 'top-[490px]'} md:opacity-100`}>
+            <ul className={`menu md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-[#f1f3f4] md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 opacity-100' : 'top-[490px]'} md:opacity-100`}>
                 {
                     Links.map((link) => (
                         <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
@@ -36,7 +38,7 @@ const Navbar = () => {
                     ))
                 }
             </ul>
-        </div>
+        </nav>
     </div>
   )
 }
