30daf76881ef6b30cb08c9248cc78c3a9e0c6d8c
JalenCameron <109976308+JalenCameron@users.noreply.github.com>
2022-11-04 23:39:17 -0700

    Edited my main message. Added a nice animation too

--- components/Main.jsx
+++ components/Main.jsx
@@ -8,7 +8,19 @@ const Main = () => {
                 <h1 className='text-xl tracking-widest sm:text-2xl lg:text-4xl'>Hi, I&apos;m</h1>
                 <h2 className='text-[#7f96bb] text-4xl sm:text-5xl lg:text-8xl py-4'>Jalen Cameron</h2>
                 <div className='horizontal-line'></div>
-                <p className='uppercase text-lg py-4 lg:text-2xl'>Front-End Web Developer</p>
+                <div className='carousel-wrapper overflow-hidden w-full h-[50px]'>
+                  <div className='carousel-mover relative bottom-[0%]'>
+                    <div className='w-full h-[50px]'>
+                      <p className='uppercase text-lg py-4 lg:text-2xl'>Front-End Web Developer</p>
+                    </div>
+                    <div className='w-full h-[50px]'>
+                        <p className='uppercase text-lg py-4 lg:text-2xl'>UI/UX Designer</p>
+                    </div> 
+                    <div className='w-full h-[50px]'>
+                        <p className='uppercase text-lg py-4 lg:text-2xl'>Wordpress Wizard</p>
+                    </div> 
+                  </div>
+                </div>
             </div>
         </div>
     </section>
