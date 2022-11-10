532647bfb385a9e4cc918ac562c84f1777a4ab08
JalenCameron <109976308+JalenCameron@users.noreply.github.com>
2022-11-09 23:36:19 -0800

    Added some more information to the header. Updated the favicon

--- components/HeadSEO.jsx
+++ components/HeadSEO.jsx
@@ -4,6 +4,8 @@ import Head from 'next/head';
 function HeadSEO() {
   return (
     <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Jalen Cameron · Web Developer</title>
         <meta
           name="description"
@@ -14,8 +16,14 @@ function HeadSEO() {
           content="Nextjs, React, Web Developer, UI/UX Designer, JavaScript, Portfolio, Projects, About"
         />
         <meta name="author" content="Jalen Cameron" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
     </Head>
   )
 }
